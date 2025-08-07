4fe2ae3e095805521f67c1f27513963a5a486dc7
Michael Magdosa <michael.magdosa.dev@gmail.com>
2025-08-07 15:08:01 +0800

    Debug - False

--- QueueAU/settings.py
+++ QueueAU/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%mhsz76ph5v_8^xl8wj$zo=wz&#-cyv_*dirc%p_p9oe#%_!ui'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['queueau-beta.onrender.com']
 
@@ -207,4 +207,5 @@ CELERY_BROKER_USE_SSL = {
 CELERY_REDIS_BACKEND_USE_SSL = {
     'ssl_cert_reqs': ssl.CERT_NONE
 }
+
 
